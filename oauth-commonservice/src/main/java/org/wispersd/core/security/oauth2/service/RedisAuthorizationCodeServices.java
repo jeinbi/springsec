1a904c933cd2138a3506eb03a5da285b62fb3367
wispershadow <zhan_jia@hotmail.com>
2017-06-21 12:41:11 +0800

    Authcode service impl

--- oauth-commonservice/src/main/java/org/wispersd/core/security/oauth2/service/RedisAuthorizationCodeServices.java
+++ oauth-commonservice/src/main/java/org/wispersd/core/security/oauth2/service/RedisAuthorizationCodeServices.java
@@ -1,33 +1,44 @@
 package org.wispersd.core.security.oauth2.service;
 
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.code.RandomValueAuthorizationCodeServices;
 
 public class RedisAuthorizationCodeServices extends RandomValueAuthorizationCodeServices{
 	private RedisTemplate<String, OAuth2Authentication> redisTemplate;
-	
-	
-	
-	public RedisTemplate<String, OAuth2Authentication> getRedisTemplate() {
+	private HashOperations<String, String, OAuth2Authentication> hashOpsForAuthCode;
+	private final String tablePrefix = "AUTHCODE";
+
+
+
+	public RedisTemplate<String, OAuth2Authentication> getRedisTemplate()
+	{
 		return redisTemplate;
 	}
 
-	public void setRedisTemplate(
-			RedisTemplate<String, OAuth2Authentication> redisTemplate) {
+	public void setRedisTemplate(final RedisTemplate<String, OAuth2Authentication> redisTemplate)
+	{
 		this.redisTemplate = redisTemplate;
 	}
 
-	@Override
-	protected void store(String code, OAuth2Authentication authentication) {
-		// TODO Auto-generated method stub
-		
+	public void init()
+	{
+		hashOpsForAuthCode = redisTemplate.opsForHash();
 	}
 
 	@Override
-	protected OAuth2Authentication remove(String code) {
-		// TODO Auto-generated method stub
-		return null;
+	protected void store(final String code, final OAuth2Authentication authentication)
+	{
+		hashOpsForAuthCode.put(tablePrefix, code, authentication);
+	}
+
+	@Override
+	protected OAuth2Authentication remove(final String code)
+	{
+		final OAuth2Authentication auth = hashOpsForAuthCode.get(tablePrefix, code);
+		hashOpsForAuthCode.delete(tablePrefix, code);
+		return auth;
 	}
 
 }
